a2c3d269002c4be2f92263740a0130b631eda0ea
Paul-Hubert Cannesson <phcannesson@deezer.com>
2026-01-07 15:39:02 +0100

    Do not use cache, measure time

--- scheduler/src/test/kotlin/ParallelRequestsTest.kt
+++ scheduler/src/test/kotlin/ParallelRequestsTest.kt
@@ -1,4 +1,8 @@
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.cache.normalized.FetchPolicy
+import com.apollographql.apollo.cache.normalized.api.MemoryCacheFactory
+import com.apollographql.apollo.cache.normalized.fetchPolicy
+import com.apollographql.apollo.cache.normalized.normalizedCache
 import com.apollographql.apollo.cache.normalized.watch
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
@@ -9,9 +13,11 @@ import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.runBlocking
 import test.HelloQuery
 import kotlin.test.Test
+import kotlin.time.TimeSource
 import kotlin.time.measureTime
 
 class ParallelRequestsTest {
@@ -24,21 +30,23 @@ class ParallelRequestsTest {
             .build()
 
         // Launch 100 parallel requests
-        val flows = (1..100).map {
-            flowOf(apolloClient.query(HelloQuery()))
+        val flows = (1..100).map { index ->
+            flowOf(
+                apolloClient
+                    .query(HelloQuery())
+                    .fetchPolicy(FetchPolicy.NetworkOnly)
+            )
                 .flatMapLatest {
                     it.watch()
                 }
+                .onEach { println("We got data for flow $index") }
         }
-        val time = measureTime {
-            combine(flows)
-            { it ->
-                //Nothing
-            }.collect {
-                println("Work finished")
-            }
+        val before = TimeSource.Monotonic.markNow()
+        combine(flows) { it ->
+            // Nothing here
+        }.collect {
+            val after = TimeSource.Monotonic.markNow()
+            println("Work finished for all workers in ${after - before}")
         }
-
-        println("Successfully executed 100 parallel requests in $time")
     }
 }
